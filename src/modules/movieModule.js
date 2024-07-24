5b5e60985969d3a6e3d62bf159019cbda748c61f
DaroOps <romerosd089@hotmail.com>
2024-07-24 15:25:12 -0500

    feat: :construction: Add MovieModule and main testing functions

---
+++ src/modules/movieModule.js
@@ -0,0 +1,42 @@
+import DbService from '../db/dbConection.js';
+
+export default class Movie {
+    static instanceMovie
+    adminClient;
+    dbService;
+    constructor(client = null) {
+        if (Movie.instanceMovie) {
+            return Movie.instanceMovie;
+        }
+        this.client = client.getClient();
+        this.dbService = new DbService(this.client);
+        Movie.instanceMovie = this;
+    }
+
+    async getMovieDetails(movieId) {
+        try {
+            const db = await this.dbService.connect();
+            const movie = await db.collection('movies').findOne({ _id: movieId });
+            return movie;
+        } catch (error) {
+            console.error('Error getting movie details:', error);
+            throw error;
+        } finally {
+            await this.dbService.close();
+        }
+    }
+
+    async  listMovies() {
+        try {
+            const db = await this.dbService.connect();
+            const movies = await db.collection('movies').find({}).toArray();
+            return movies;
+        } catch (error) {
+            console.error('Error listing movies:', error);
+            throw error;
+        } finally {
+            await this.dbService.close();
+        }
+    }
+}
+
