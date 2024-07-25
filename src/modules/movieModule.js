86dc85cd9afe01c75a29132eca9921aefaf1ffeb
DaroOps <romerosd089@hotmail.com>
2024-07-25 13:07:47 -0500

    feat: :bulb: Add code documentation

--- src/modules/movieModule.js
+++ src/modules/movieModule.js
@@ -14,6 +14,22 @@ export default class Movie {
         Movie.instanceMovie = this;
     }
 
+    /**
+     * Retrieves the details of a movie from the database.
+     *
+     * @param {string} movieId - The unique identifier of the movie.
+     *
+     * @returns {Promise<Object>} A promise that resolves to a movie object.
+     * The movie object contains the following properties:
+     * - _id: The unique identifier of the movie.
+     * - title: The title of the movie.
+     * - genre: The genre of the movie.
+     * - duration: The duration of the movie in minutes.
+     * - synopsis: The synopsis of the movie.
+     * - screening_times: An array of screening times for the movie.
+     *
+     * @throws {Error} If an error occurs while retrieving the movie details.
+     */
     async getMovieDetails(movieId) {
         try {
             const db = await this.dbService.connect();
@@ -27,6 +43,20 @@ export default class Movie {
         }
     }
 
+    /**
+     * Retrieves a list of all movies from the database.
+     *
+     * @returns {Promise<Array>} A promise that resolves to an array of movie objects.
+     * Each movie object contains the following properties:
+     * - _id: The unique identifier of the movie.
+     * - title: The title of the movie.
+     * - genre: The genre of the movie.
+     * - duration: The duration of the movie in minutes.
+     * - synopsis: The synopsis of the movie.
+     * - screening_times: An array of screening times for the movie.
+     *
+     * @throws {Error} If an error occurs while retrieving the movies.
+     */
     async  listMovies() {
         try {
             const db = await this.dbService.connect();
