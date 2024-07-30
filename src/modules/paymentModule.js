8fe1545fb10d5cfbfa5dd92c93f9c5327ded791c
DaroOps <romerosd089@hotmail.com>
2024-07-29 22:18:21 -0500

    feat: :bulb: Fix outdated documentation

--- src/modules/paymentModule.js
+++ src/modules/paymentModule.js
@@ -15,6 +15,18 @@ export default class Payment {
     Payment.instancePayment = this;
   }
 
+  /**
+  * Processes a payment for a given ticket and updates the ticket status to 'paid'.
+  * It also inserts a new payment record into the 'payments' collection.
+  *
+  * @param {string} ticketId - The unique identifier of the ticket for which the payment is being processed.
+  * @param {object} paymentInfo - The payment information provided by the user.
+  *
+  * @returns {Promise<object>} - A promise that resolves to the payment object if the payment is successful.
+  * If the payment fails, the promise rejects with an error.
+  *
+  * @throws {Error} - Throws an error if the ticket is not found or if the ticket is already paid.
+  */
   async processPayment(ticketId, paymentInfo) {
     const db = await this.dbService.connect();
     const session = await db.client.startSession();
@@ -61,6 +73,21 @@ export default class Payment {
     }
   }
 
+  /**
+  * Confirms the purchase of a ticket by checking its existence in the database.
+  *
+  * @param {string} ticketId - The unique identifier of the ticket to be confirmed.
+  *
+  * @returns {Promise<boolean>} - A promise that resolves to true if the ticket is found,
+  * indicating a successful purchase confirmation. If the ticket is not found, the promise
+  * rejects with an error.
+  *
+  * @throws {Error} - Throws an error if the ticket is not found.
+  * 
+  * @note This is a dumb function cause don't perform any operation in the database
+  * 
+  * @deprecated
+  */
   async confirmPurchase(ticketId) {
     const db = await this.dbService.connect();
     try {
@@ -76,6 +103,21 @@ export default class Payment {
     }
   }
 
+  /**
+   * Applies a discount to a ticket based on the provided discount type.
+   *
+   * @param {string} ticketId - The unique identifier of the ticket to apply the discount to.
+   * @param {string} discountType - The type of discount to apply.
+   *
+   * @returns {Promise<boolean>} - A promise that resolves to true if the discount is successfully applied,
+   * indicating that the ticket's price has been updated. If the discount is not found or cannot be applied,
+   * the promise resolves to false.
+   *
+   * @throws {Error} - Throws an error if there is an issue connecting to the database,
+   * applying the discount, or closing the database connection.
+   *
+   * @async
+   */
   async applyDiscount(ticketId, discountType) {
     const db = await this.dbService.connect();
     const session = await db.client.startSession();
@@ -116,6 +158,19 @@ export default class Payment {
     }
   }
 
+  /**
+  * Verifies if a user has a valid VIP card and applies a VIP discount to the ticket if applicable.
+  *
+  * @param {string} userId - The unique identifier of the user.
+  * @param {string} ticketId - The unique identifier of the ticket to apply the discount to.
+  *
+  * @returns {Promise<boolean>} - A promise that resolves to true if the VIP discount is successfully applied,
+  * false if the user doesn't have a valid VIP card or if the discount couldn't be applied.
+  *
+  * @throws {Error} - Throws an error if there's an issue with database operations or if the VIP discount is not found.
+  *
+  * @async
+  */
   async verifyVIPCardAndApplyDiscount(userId, ticketId) {
     const db = await this.dbService.connect();
     const session = await db.client.startSession();
