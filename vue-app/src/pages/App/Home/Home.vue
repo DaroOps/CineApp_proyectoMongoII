e2175a0dd86cd7d2fe3d55256e7e84ae8616bc23
DaroOps <romerosd089@hotmail.com>
2024-08-13 20:47:21 -0500

    feat: :construction: Add head for the home view

--- vue-app/src/pages/App/Home/Home.vue
+++ vue-app/src/pages/App/Home/Home.vue
@@ -1,13 +1,24 @@
 <script setup>
 import MoviesCarousel from './components/MoviesCarousel.vue'
-
+import SeeAll from './components/SeeAll.vue'
+import CustomInput from './components/CustomInput.vue';
+import ProfileHead from './components/ProfileHead.vue'
 
 </script>
 
 <template>
     <div class="home">
-        <div>home view</div>
-        <MoviesCarousel > </MoviesCarousel>
+        <div class="head">
+            <div class="profile">
+                <ProfileHead name="John Doe" profilePicUrl="https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.jpg"/>
+                <CustomInput/>
+            </div>
+            <SeeAll/>
+        </div>
+        <MoviesCarousel />
+        <div class="botom">
+            <SeeAll/>
+        </div>
     </div>
 </template>
 
@@ -15,5 +26,24 @@ import MoviesCarousel from './components/MoviesCarousel.vue'
     .home {
         height: 100vh;
         width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .head{
+        padding: 30px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .profile{
+        display: flex;
+        flex-direction: column;
+        gap: 17px;
+        width: 100%;
+    }
+    
+    .botom{
+        padding: 30px;
     }
 </style>
---
+++ vue-app/src/pages/App/Home/components/CustomInput.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="search-container">
+      <input
+        type="text"
+        class="search-input"
+        placeholder="Search movie, cinema, genre..."
+        v-model="searchQuery"
+        @input="onInput"
+      >
+      <span class="search-icon">
+        <IconBar/>
+      </span>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import IconBar from '@icons/nav/IconBar.vue'
+
+  const searchQuery = ref('')
+  
+  const onInput = () => {
+    console.log('Search query:', searchQuery.value)
+  }
+  </script>
+  
+  <style lang ="scss" scoped>
+  .search-container {
+    position: relative;
+    width: 100%;
+    
+
+  }
+  
+  .search-input {
+   
+    width: 100%;
+    padding: 15px 15px 15px 40px;
+    border-radius: 10px;
+    border: 0;
+    outline: var(--text-color) 1px solid;
+    background: var(--background-linear);
+    color: var(--text-color-rgb);
+    font-size: 14px;
+
+  }
+  
+  .search-input::placeholder {
+    color: var(--text-color);
+  }
+
+  .search-input:focus{
+    outline: var(--text-color) 1px solid;
+   }
+
+  
+  .search-icon {
+    position: absolute;
+    width: 14px;
+    height: auto;
+    left: 15px;
+    top: 52%;
+    transform: translateY(-50%);
+    color: var(--text-color-rgb/70%);
+  }
+  </style>
