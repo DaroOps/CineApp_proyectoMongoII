69070dd5dcdc9cc5ebee4bafc67ce6c24dac44d6
DaroOps <romerosd089@hotmail.com>
2024-08-15 00:08:59 -0500

    feat: :truck: Rename store for better reading

--- vue-app/src/stores/movies.js
+++ vue-app/src/stores/movies.js
@@ -1,9 +1,15 @@
 import { defineStore } from 'pinia'
 import axios from 'axios';
 
-export const useCarouselStore = defineStore('carousel', {
+export const useMovieStore = defineStore('movies', {
   state: () => ({
     movies: [],
+    selectedMovie: {
+      title: '',
+      genre: '',
+      image_url: '',
+      _id: ''
+    },
     currentIndex: 0
   }),
   actions: {
@@ -16,10 +22,20 @@ export const useCarouselStore = defineStore('carousel', {
     async fetchMovies() {
       const {data} = await axios.get(`http://localhost:3000/api/movies`)
       this.movies = data;
+    },
+    async fetchMovieDetails(movieId) {
+      const {data} = await axios.get(`http://localhost:3000/api/movies/${movieId}`)
+      this.selectedMovie = data;
+    },
+    setSelectedMovie(movie) {
+      this.selectedMovie = movie ? { ...movie } : null;
+    },
+    clearSelectedMovie() {  
+      this.selectedMovie = {};
     }
   },
   getters: {
     moviesArray: (state) => state.movies,
-    currentImageIndex: (state) => state.currentIndex
-  }
+    currentImageIndex: (state) => state.currentIndex,
+  },
 })
