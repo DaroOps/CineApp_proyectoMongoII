1fdd4d45bececcc99a097589c03d39296c396217
DaroOps <romerosd089@hotmail.com>
2024-08-16 21:14:15 -0500

    feat: :recycle: Now vip is on the ecuation

--- vue-app/src/pages/App/Book/components/SeatMap.vue
+++ vue-app/src/pages/App/Book/components/SeatMap.vue
@@ -8,6 +8,14 @@ import IconScreen from '@icons/static/IconScreen.vue';
 const screeningStore = useScreeningStore()
 const { seats, selectedSeats } = storeToRefs(screeningStore)
 const { toggleSeat } = screeningStore
+
+const tagsArray = [
+  { id: 1, text: 'Available' , color: 'var(--background-color2)' },
+  { id: 2, text: 'Reserved' , color: 'var(--seat-reserved-color)' },
+  { id: 3, text: 'Selected' , color: 'var(--primary-color)' },
+  { id: 4, text: 'VIP' , color: 'var(--vip-color)' },
+];
+
 </script>
 
 <template>
@@ -21,7 +29,11 @@ const { toggleSeat } = screeningStore
         <button 
           v-for="seat in row" 
           :key="seat.id"
-          :class="['seat', { 'disabled': seat.disabled, 'selected': selectedSeats.includes(seat.id) }]"
+          :class="['seat', { 
+            'disabled': seat.disabled,
+            'selected': selectedSeats.includes(seat.id),
+            'vip': seat.isVIP && selectedSeats.includes(seat.id)
+          }]"
           @click="toggleSeat(seat.id)"
           :disabled="seat.disabled"
           >
@@ -29,10 +41,16 @@ const { toggleSeat } = screeningStore
         </button>
       </div>
     </div>
+    <div class="seat-map-footer">
+      <div v-for="tag in tagsArray" :key="tag.id" class="tag">
+        <div :style="{ backgroundColor: tag.color }" class="dot"/>
+        <span class="tag-text">{{ tag.text }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .seat-map {
   display: flex;
   flex-direction: column;
@@ -96,4 +114,35 @@ const { toggleSeat } = screeningStore
   font-family: poppins;
   font-size: 18px;
 }
+
+.seat.vip {
+  background-color: var(--vip-color);
+}
+
+.seat-map-footer {
+  display: flex;
+  gap: 31px;
+  justify-content: center;
+  margin-left: 0px;
+  margin-top: 32px;
+
+  & .tag{
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    & .dot{
+      width: 12px;
+      height: 12px;
+      border-radius: 50%;
+    }
+    & .tag-text{
+      font-size: 12px;
+      font-family: var(--font-inter);
+      font-weight: 500;
+      color: var(--text-color);
+    }
+  }
+  
+}
+
 </style>
